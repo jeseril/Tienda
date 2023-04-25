caa4733a320b937f2566f12da88e3354171b274a
Jesus Rincon <jeseril2213@gmail.com>
2023-04-24 21:14:49 -0500

    mobile-menu

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobile_menu = document.querySelector('.mobile-menu')
+const btn_mobile_menu = document.querySelector('.btn-mobile-menu')
 
 navEmail.addEventListener('click',toggleDesktopMenu)
+btn_mobile_menu.addEventListener('click',toggleMobileMenu)
 
 function toggleDesktopMenu () {
     console.log('click');
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu () {
+    console.log('click');
+    mobile_menu.classList.toggle('inactive');
+}
+
